1f9573d2690887aa4b7e75eb84ae669132c74ac0
Huy Phi Hoang <phihoanghuy@phis-macbook-pro.local>
2018-10-29 22:19:58 +1100

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Phi Hoang Huy on 10/29/18.
+//  Copyright © 2018 Phi Hoang Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
